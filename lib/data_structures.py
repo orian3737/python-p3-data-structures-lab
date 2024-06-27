5c871a00ef328aeaf1f308e67cbf3ba15a2487dc
Ryan Murzyn <orian3737@gmail.com>
2024-06-27 12:56:23 -0400

    fun

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,64 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
+print(get_names(spicy_foods))
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
+print(get_spiciest_foods(spicy_foods))
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        heat_emoji = "🌶" * heat_level
+        print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
+        
+print_spicy_foods(spicy_foods)
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+print(get_spicy_food_by_cuisine(spicy_foods, "American")) 
+print(get_spicy_food_by_cuisine(spicy_foods, "Thai"))
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spicest_foods =  [food for food in spicy_foods if food["heat_level"] > 5]
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        heat_emoji = "🌶" * heat_level
+        print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
+        return spicest_foods
+ 
+print_spiciest_foods(spicy_foods)
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    average_heat_level = total_heat_level / len(spicy_foods)
+    return int(average_heat_level)
+
+print(get_average_heat_level(spicy_foods))
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+new_food = {
+    "name": "Salsa",
+    "cuisine": "Mexican",
+    "heat_level": 7,
+}
+
+print(create_spicy_food(spicy_foods, new_food))
