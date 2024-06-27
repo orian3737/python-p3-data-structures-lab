cf13023ba0101bb24255f673237b84d649002048
Ryan Murzyn <orian3737@gmail.com>
2024-06-27 13:04:48 -0400

    ok

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,5 @@
+# lib/data_structures.py
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -18,14 +20,9 @@ spicy_foods = [
 
 def get_names(spicy_foods):
     return [food["name"] for food in spicy_foods]
-print(get_names(spicy_foods))
-
 
 def get_spiciest_foods(spicy_foods):
     return [food for food in spicy_foods if food["heat_level"] > 5]
-
-print(get_spiciest_foods(spicy_foods))
-
 
 def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
@@ -34,42 +31,37 @@ def print_spicy_foods(spicy_foods):
         heat_level = food["heat_level"]
         heat_emoji = "🌶" * heat_level
         print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
-        
-print_spicy_foods(spicy_foods)
 
+def print_spiciest_foods(spicy_foods):
+    spiciest_foods = [food for food in spicy_foods if food["heat_level"] > 5]
+    for food in spiciest_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        heat_emoji = "🌶" * heat_level
+        print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
         if food["cuisine"] == cuisine:
             return food
 
-print(get_spicy_food_by_cuisine(spicy_foods, "American")) 
-print(get_spicy_food_by_cuisine(spicy_foods, "Thai"))
-
-
-def print_spiciest_foods(spicy_foods):
-    spicest_foods =  [food for food in spicy_foods if food["heat_level"] > 5]
-    for food in spicy_foods:
-        name = food["name"]
-        cuisine = food["cuisine"]
-        heat_level = food["heat_level"]
-        heat_emoji = "🌶" * heat_level
-        print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
-        return spicest_foods
- 
-print_spiciest_foods(spicy_foods)
-
-
 def get_average_heat_level(spicy_foods):
     total_heat_level = sum(food["heat_level"] for food in spicy_foods)
     average_heat_level = total_heat_level / len(spicy_foods)
     return int(average_heat_level)
 
-print(get_average_heat_level(spicy_foods))
-
-def create_spicy_food(spicy_foods, spicy_food):
-    spicy_foods.append(spicy_food)
+def create_spicy_food(spicy_foods, new_spicy_food):
+    spicy_foods.append(new_spicy_food)
     return spicy_foods
+
+print(get_names(spicy_foods))
+print(get_spiciest_foods(spicy_foods))
+print_spicy_foods(spicy_foods)
+print_spiciest_foods(spicy_foods)
+print(get_spicy_food_by_cuisine(spicy_foods, "American"))
+print(get_spicy_food_by_cuisine(spicy_foods, "Thai"))
+print(get_average_heat_level(spicy_foods))
 
 new_food = {
     "name": "Salsa",
